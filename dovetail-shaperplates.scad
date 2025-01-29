7d1159a1981c47f4e476b84ca4c42f79a8fd2284
Jordan Powers <jordanpowers1227@gmail.com>
2025-01-29 01:10:39 -0800

    Add dominos

--- dovetail-shaperplates.scad
+++ dovetail-shaperplates.scad
@@ -5,6 +5,7 @@ thickness = (1/16) * 25.4;
 
 dovetail_depth = 15;
 dovetail_radius = 1;
+domino_thickness = 0.4;
 
 domino_width = 43;
 domino_height = 12.7;
@@ -53,17 +54,30 @@ module dovetail() {
         }
     }
 }
-union() {
+
+module dominos(fid) {
+    translate([0, height - dovetail_depth, thickness-domino_thickness])
+    linear_extrude(domino_thickness)
+    scale(25.4/96)
     difference() {
+        import(str(fid, "_outline.svg"));
+        import(str(fid, "_dots.svg"));
+    }
+}
+
+difference() {
+    union() {
         cube([width - dovetail_depth, height - dovetail_depth, thickness], false);
         for (i = [0 : 2 : num_rows-3] )
-            translate([0, i*(domino_spacing + domino_height) + domino_height + dovetail_depth + padding_height, 0]) dovetail();
-        
-        for (i = [0 : 2 : floor((width - (dovetail_depth*2)) / dovetail_width)-1])
-            translate([dovetail_width * (i+1) + dovetail_depth, 0, 0]) rotate([0, 0, 90]) dovetail();
-    }
-     for (i = [0 : 2 : num_rows-3] )
             translate([width - dovetail_depth, i*(domino_spacing + domino_height) + domino_height + dovetail_depth + padding_height, 0]) dovetail();
-     for (i = [0 : 2 : floor((width - (dovetail_depth*2)) / dovetail_width)-1])
+        for (i = [0 : 2 : floor((width - (dovetail_depth*2)) / dovetail_width)-1])
             translate([dovetail_width * (i+1) + dovetail_depth, height - dovetail_depth, 0]) rotate([0, 0, 90]) dovetail();
- }
+    }
+    for (i = [0 : 2 : num_rows-3] )
+        translate([0, i*(domino_spacing + domino_height) + domino_height + dovetail_depth + padding_height, 0]) dovetail();
+    
+    for (i = [0 : 2 : floor((width - (dovetail_depth*2)) / dovetail_width)-1])
+        translate([dovetail_width * (i+1) + dovetail_depth, 0, 0]) rotate([0, 0, 90]) dovetail();
+    #dominos(1);
+}
+     
---
+++ gen-domino-svgs.py
@@ -0,0 +1,119 @@
+import argparse
+from random import randint
+from svgwrite import Drawing
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("width", type=int, help="width of 3d printer build plate, in mm")
+parser.add_argument("height", type=int, help="height of 3d printer build plate, in mm")
+parser.add_argument("dovetail_depth", type=int, help="depth of dovetail, in mm")
+parser.add_argument("num_plates", type=int, help="how many unique plate patterns to generate")
+
+args = parser.parse_args()
+
+domino_spacing = 5
+domino_width = 43
+domino_height = 12.7
+# dovetail_angle = max(math.radians(60), math.atan(args.dovetail_depth / domino_spacing))
+
+num_rows = int((args.height - (args.dovetail_depth*2) + domino_spacing) // (domino_height + domino_spacing))
+print(f"num_rows={num_rows}")
+
+padding_height = (args.height - (args.dovetail_depth*2) - ((num_rows * (domino_height + domino_spacing)) - domino_spacing)) / 2
+print(f"padding_height={padding_height}")
+
+num_cols = int((args.width - args.dovetail_depth - domino_spacing) // (domino_width + domino_spacing))
+print(f"num_cols={num_cols}")
+
+padding_width = (args.width - args.dovetail_depth - (num_cols * (domino_width + domino_spacing))) / 2
+print(f"padding_width={padding_width}")
+
+# Adapted from https://github.com/augiev/Shaper-Dominos
+dpi = 96
+scale = dpi/25.4
+fillet_rad = 2.5*scale
+circle_rad = 1.25*scale
+circle_grid = 5.08*scale
+rect_w = domino_width*scale
+rect_h = domino_height*scale
+
+border = 5*scale
+xoff = rect_w/2 + padding_width*scale
+yoff = rect_h/2 + padding_height*scale
+
+def generate(d_outline, d_dots, rows, cols, previous=[]):
+    vals = []
+    while len(vals) < (rows*cols):
+        rand = randint(0, 65536)
+        rand |= 1 | (1<<7) | (1<<8) | (1<<15)
+        top = bin(rand)[3:9]
+        bottom = bin(rand)[11:17]
+
+        # require 10 total dots
+        if bin(rand).count('1') != 10:
+            continue
+
+        # discard if dots are in a rotationally-symmetric pattern
+        if top == ''.join(reversed(bottom)):
+            continue
+
+        # check if an exact duplicate exists
+        if not rand in vals and not rand in previous:
+            vals.append(rand)
+
+
+    row_col = []
+    for r in range(rows):
+        for c in range(cols):
+            row_col.append((r,c))
+    gen = zip(row_col, vals)
+
+    for (r, c), spots in gen:
+        xoff2 = xoff + c*(rect_w + border)
+        yoff2 = yoff + r*(rect_h + border)
+
+        geo_r = r
+        if geo_r > 0 and geo_r % 2 == 0:
+            xoff2 += args.dovetail_depth*scale
+
+
+        # draw rounded rectangle
+        cmds = [
+            'M {} {}'.format(-rect_w/2 + fillet_rad + xoff2, rect_h/2 + yoff2),
+            'A{},{} 0 0,1 {},{}'.format(fillet_rad, fillet_rad, -rect_w/2 + xoff2, rect_h/2 - fillet_rad + yoff2),
+            'L {} {}'.format(-rect_w/2 + xoff2, -rect_h/2 + fillet_rad + yoff2),
+            'A{},{} 0 0,1 {},{}'.format(fillet_rad, fillet_rad, -rect_w/2 + fillet_rad + xoff2, -rect_h/2 + yoff2),
+            'L {} {}'.format(rect_w/2 - fillet_rad + xoff2, -rect_h/2 + yoff2),
+            'A{},{} 0 0,1 {},{}'.format(fillet_rad, fillet_rad, rect_w/2 + xoff2, -rect_h/2 + fillet_rad + yoff2),
+            'L {} {}'.format(rect_w/2 + xoff2, rect_h/2 - fillet_rad + yoff2),
+            'A{},{} 0 0,1 {},{}'.format(fillet_rad, fillet_rad, rect_w/2 - fillet_rad + xoff2, rect_h/2 + yoff2),
+            'z'
+            ]
+
+        d_outline.add(d_outline.path(d=''.join(cmds), fill='black', stroke='none'))
+
+        for i in range(8):
+            if spots & (1<<i):
+                d_dots.add(d_dots.circle(center=((-3.5 + i)*circle_grid + xoff2, circle_grid/2 + yoff2), r=circle_rad, fill='black'))
+            if spots & (1<<(i+8)):
+                d_dots.add(d_dots.circle(center=((-3.5 + i)*circle_grid + xoff2, -circle_grid/2 + yoff2), r=circle_rad, fill='black'))
+    return vals
+
+
+previous = []
+
+max_count = 903 # total number of valid fiducials (based on exhaustive search)
+
+requested_count = num_rows*num_cols*args.num_plates
+if requested_count > max_count:
+    print(f'error, only {max_count} fiducials exist; requested {requested_count}')
+    exit(1)
+
+for i in range(args.num_plates):
+    outpath_outline = Path.cwd() / f'{i}_outline.svg'
+    outpath_dots = Path.cwd() / f'{i}_dots.svg'
+    dwg_outline = Drawing(filename=str(outpath_outline), debug=True)
+    dwg_dots = Drawing(filename=str(outpath_dots), debug=True)
+    previous.extend(generate(dwg_outline, dwg_dots, num_rows, num_cols, previous))
+    dwg_outline.save()
+    dwg_dots.save()
